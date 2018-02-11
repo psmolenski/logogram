a3bdd44e87e73c1514cf8a0df7b89396f283ec51
Paweł Smoleński <pwl.smolenski@gmail.com>
2018-02-11 13:42:49 +0100

    Removed blue highlight on touch devices

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import * as angular from "angular";
 import logogram from "./logogram.module";
+import './main.less';
 
 angular.element(document).ready(() => {
   angular.bootstrap(document, [logogram.name]);
