aadb72ccbfcba1a8a68b1665271f0f7473ba7a98
Paweł Smoleński <pwl.smolenski@gmail.com>
2018-02-20 21:35:44 +0100

    Added more 10x10 grids

--- app/data/grids.ts
+++ app/data/grids.ts
@@ -3,16 +3,19 @@ export type GridPattern = number[][];
 export interface GridPatternGroup {
     name: string;
     patterns: GridPattern[];
+    gridSize: string;
 }
 
 const gridGroups : GridPatternGroup[] = [
     {
         name: '5x5',
-        patterns: require('./5x5.json')
+        patterns: require('./5x5.json'),
+        gridSize: 'small'
     },
     {
         name: '10x10',
-        patterns: require('./10x10.json')
+        patterns: require('./10x10.json'),
+        gridSize: 'medium'
     },
 ];
 
